567409f2f084203a76b15e2d99f1d86ec5142197
unknown <jemitvaghasiya07@gmail.com>
2026-08-16 23:32:48 +0530

    PR4 coomitted

--- Practical 4/backend/middleware/logger.js
+++
@@ -1,7 +0,0 @@
-// Logs method, URL, and timestamp for every incoming request
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url} - ${new Date().toISOString()}`);
-  next();
-};
-
-module.exports = logger;
